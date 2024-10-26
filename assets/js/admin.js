ad6a55a3b288e0f8bf764bf77fd47cb0078baa9f
Nazmul Hasan Robin <robin.sust08@gmail.com>
2024-10-26 11:43:46 +0600

    Modal close on outside click

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,5 +1,15 @@
 ;(function($) {
-    
     // console.log('Admin js');
 
+    $(document).on('click', function(event) {
+        if ($(event.target).closest('.fbs_board_section__task_lists').length) {
+            return; // Exit if the click is inside .fbs_board_section__task_lists or its child
+        }
+
+        let $modal = $('.fbs_view_task_modal:visible');
+        
+        if ($modal.length && !$modal.is(event.target) && $modal.has(event.target).length === 0) {
+            $(document).find('.fbs-close-task-modal').trigger('click');
+        }
+    });
 })(jQuery);
